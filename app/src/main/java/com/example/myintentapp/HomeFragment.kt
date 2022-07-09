4d46bca48aed9df83f674ae774667a5a0db5b4b0
DevaJayantha <devajayantha888@gmail.com>
2022-07-09 23:12:02 +0800

    add new fragment in fragment activity

--- app/src/main/java/com/example/myintentapp/HomeFragment.kt
+++ app/src/main/java/com/example/myintentapp/HomeFragment.kt
@@ -28,6 +28,16 @@ class HomeFragment : Fragment(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+        if (v != null) {
+            if (v.id == R.id.btn_category) {
+                val mCategoryFragment = CategoryFragment()
+                val mFragmentManager = parentFragmentManager
+                mFragmentManager.beginTransaction().apply {
+                    replace(R.id.frame_container, mCategoryFragment, CategoryFragment::class.java.simpleName)
+                    addToBackStack(null)
+                    commit()
+                }
+            }
+        }
     }
 }
