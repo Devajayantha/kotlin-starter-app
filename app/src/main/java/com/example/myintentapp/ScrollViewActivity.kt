47315d354f27462cfea44ad5cdfa4c3719047303
DevaJayantha <devajayantha888@gmail.com>
2022-07-10 22:53:48 +0800

    exercise scroll view

--- app/src/main/java/com/example/myintentapp/ScrollViewActivity.kt
+++ app/src/main/java/com/example/myintentapp/ScrollViewActivity.kt
@@ -7,5 +7,7 @@ class ScrollViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll_view)
+
+        supportActionBar?.title = "Google Pixel"
     }
 }
